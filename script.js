e1d595cc07e8fb7156a7106128c39e294b0cea1a
FutureTech <zafarhabibsindhi2511@gmail.com>
2026-01-17 12:37:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ e.preventDefault();
 document.getElementById("certMsg").innerText =
 "Certificate not found or course not completed yet.";
 }
+function submitCourse(e){
+  e.preventDefault();
+
+  alert(
+    "Enrollment submitted successfully!\n\n" +
+    "Your payment receipt will be verified by admin.\n" +
+    "Certificate will be enabled after course completion."
+  );
+
+  document.getElementById("courseForm").reset();
+}
+
